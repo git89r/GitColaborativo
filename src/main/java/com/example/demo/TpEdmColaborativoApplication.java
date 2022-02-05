f6fb422d9b2fe1837417515822fe8eab1dce54a4
Alumno <Alumno@Sofia>
2022-02-05 18:10:19 -0300

    -agregado de clases

---
+++ src/main/java/com/example/demo/TpEdmColaborativoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpEdmColaborativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpEdmColaborativoApplication.class, args);
+	}
+
+}
